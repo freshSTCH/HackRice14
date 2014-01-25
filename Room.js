95e128f793a1b6b1062bcac70fd4bb221d84fef3
Clayton Drazner <Drazcmd@gmail.com>
2014-01-25 14:14:02 -0600

    Making sure I don't fuck everyone up with globals!

--- Room.js
+++ Room.js
@@ -1,36 +1,36 @@
-function Room(Enemies, FurnitureList, SpecialTiles, SizeRoom)
+function Room(xLength,yLength,Enemies,Tiles)
 {
-
+	"use strict";
+	/*
 	Has a function to take in pixel locations, multiply to
 	figure out where on the grid that actually is.
 	*/
-	var grid=        ;
+	var grid=[];
 	var Enemies=Enemies
+	var Tiles=Tiles
+
+	var initializeGrid()=function(){
+		var i=0;
+		while (i<SizeRoom):
+			var j=0;
+			while (j<SizeRoom):
+				grid[i][j] = ["Empty";		
+	}
+
 	var addEnemy=function(Enemy,x,y){
-		grid[x,y] = [Enemy,angle]
+		grid[x][y] = Enemy
 	};
 	var addTile=function(Tile,x,y){
-		grid[x,y] = [Tile]
+		grid[x][y] = Tile
 	};
 	var getGrid=function(){
 		return grid
 	};
-
-/*STILL IN PROGRESS - DONT JUDGE ^^/*	
-	var initializeGraph(SizeRoom){
-	/*initialize the grid - we take in a size, then make a square grid 
-	of that size squared(checkthis?). */
-		i=0;
-		while (i<SizeRoom):
-			j=0;
-			while (j<SizeRoom):
-				grid[i[j]] = ["Empty", Null];		
-
-		//put the initial Enemies on the grid
-		for (Enemy in Enemies):
-			grid[Enemy[],Enemy[]]=Enemy[0]
-
+	var checkSpot=function(pixelX, pixelY){
+		//check what is on the grid at the given pixel location
+		//double check this formula works
+		var gridX=pixelX/floor(xLength) 
+		var gridY=floor(pixelY/(yLength))
+		return grid[gridX,gridY]
 	}
-
-
-var CurrentRoom=Room()
+}
