a11a8619216dc7e33917c91f50c1b63ee63cef88
HunterTidwell <wht1@rice.edu>
2014-01-25 13:00:56 -0600

    added geometry

---
+++ geometry.js
@@ -0,0 +1,27 @@
+function Point(x, y)
+{
+    var x = x, var y = y;
+
+    function isInRect(rect)
+    {
+        var c1 = Math.abs(x - rect.x) < 0.5 * rect.width;
+        var c2 = Math.abs(y - rect.y) < 0.5 * rect.height;
+        return c1 && c2
+    }
+
+    return {x:x, y:y, isInRect:isInRect};
+}
+
+function Rect(x, y, width, height, angle)
+{
+    var x = x, y = y, width = width, height = height, angle = angle;
+
+    function isInRect(rect)
+    {
+        var c1 = Math.abs(x - rect.x) < 0.5 * (width + rect.width);
+        var c2 = Math.abs(y - rect.y) < 0.5 * (height + rect.height);
+        return c1 && c2
+    }
+
+    return {x:x, y:y, width:width, w:width, height:height, h:height, angle:angle, a:angle, isInRect:isInRect};
+}
