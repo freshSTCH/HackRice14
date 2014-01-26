24ee2311af8968c9dce5909e6fb7e98d0d6be4b8
kgmstwo <kgmstwo@gmail.com>
2014-01-25 19:58:09 -0600

    room is now 'global'

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
 var assets;
+var room;
 GameLoader(function(a){
     assets = a;
     Menu(function() {
         var name = "1";
-        var room = RoomLoader(name);
+        room = RoomLoader(name);
 
         var loop = function(){
             room.update();
