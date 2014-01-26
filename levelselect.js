86ef08cb80eef4880f9e2ee0668eed8bcced3029
Clayton Drazner <Drazcmd@gmail.com>
2014-01-25 19:17:47 -0600

    level select in progress

---
+++ levelselect.js
@@ -0,0 +1,51 @@
+function Menu(callback)
+{
+    "use strict";
+
+    function convertToRect(objData)
+    {
+        for (var key in objData)
+            objData[key] = Rect(objData[key][0],objData[key][1]);
+    }
+
+    function getButtonPressed(buttons,pos)
+    {
+        for (var key in buttons)
+        {
+            if (buttons[key].intersectsPoint(pos))
+                return key;
+        }
+    }
+
+    GetJSON("Data/menu.json",function(buttons)
+    {
+
+        convertToRect(buttons);
+
+        var menuImage = assets.getImage("Menu");
+        canvas.drawImage(menuImage,0,0,WIDTH,HEIGHT);
+
+        var id = canvas.addMouseDownListener(function(pos)
+        {
+            var buttonPressed = getButtonPressed(buttons,pos);
+            console.log(buttonPressed);
+            if(buttonPressed)
+            {
+                switch(buttonPressed)
+                {
+                    case "playButton":
+                        canvas.removeMouseDownListener(id);
+                        callback();
+                        break; 
+
+                    default:
+                        console.error("A non existant button ",buttonPressed);
+                        break;
+                }
+            }
+        });
+
+
+    });
+
+}
