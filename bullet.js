9021c5f90c40d7942a6cee86a4c1a209c5d937d1
kgmstwo <kgmstwo@gmail.com>
2014-01-26 04:43:36 -0600

    bullet now has proper states

--- bullet.js
+++ bullet.js
@@ -46,52 +46,56 @@ var Bullet = function(pos, velocity, img, growth, rgb){
     }
 
     var draw = function(){
-        if(active){
-            canvas.putImageEasy(rect,img);
-        }else{
-            if(rgba[3] > .05){
-                canvas.save()
+        switch(state){
+            case 'active':
+            case 'stuck':
+                canvas.putImageEasy(rect,img);
+                break;
+            case 'ring':
+                if(rgba[3] > .05){
+                    canvas.save();
 
                     canvas.beginPath();
-                canvas.arc(room.offset[0] + TILESIZE * pos[0], room.offset[1] + TILESIZE * pos[1], radius, 0, 2 * Math.PI);
+                    canvas.arc(room.offset[0] + TILESIZE * pos[0], room.offset[1] + TILESIZE * pos[1], radius, 0, 2 * Math.PI);
 
-                canvas.lineWidth = 15;
-                canvas.strokeStyle = 'rgba(0,0,0,' + .25 * rgba[3] + ')';
-                        canvas.stroke();
+                    canvas.lineWidth = 15;
+                    canvas.strokeStyle = 'rgba(0,0,0,' + (.25 * rgba[3]) + ')';
+                    canvas.stroke();
 
-                        canvas.lineWidth = 3;
-                        canvas.strokeStyle = 'rgba(' + rgba[0] + ',' + rgba[1] + ',' + rgba[2] + ',' + rgba[3] +')';
-                            canvas.stroke();
+                    canvas.lineWidth = 3;
+                    canvas.strokeStyle = 'rgba(' + rgba[0] + ',' + rgba[1] + ',' + rgba[2] + ',' + rgba[3] +')';
+                    canvas.stroke();
 
-                            canvas.strokeStyle = 'rgba(' + rgba[0] + ',' + rgba[1] + ',' + rgba[2] + ',' + .5 * rgba[3] +')';
-                                canvas.lineWidth = 15;
-                                /*
-                                //turns out shadows are ugly
-                                canvas.shadowColor = 'black'
-                                canvas.shadowOffsetY = 10;
-                                canvas.shadowBlur = 5;
-                                */
-                                canvas.stroke();
+                    canvas.strokeStyle = 'rgba(' + rgba[0] + ',' + rgba[1] + ',' + rgba[2] + ',' + .5 * rgba[3] +')';
+                    canvas.lineWidth = 15;
+                    /*
+                    //turns out shadows are ugly
+                    canvas.shadowColor = 'black'
+                    canvas.shadowOffsetY = 10;
+                    canvas.shadowBlur = 5;
+                    */
+                    canvas.stroke();
 
 
-                                canvas.restore();
-                                }
-                                }
-                                }
+                    canvas.restore();
+                }
+                break;
+        }
+    };
 
-                                var hit = function(obj){
-                                    hitObj = obj
-                                active = false;
-                                }
+    var hit = function(obj){
+        hitObj = obj
+    active = false;
+    }
 
-                                var reverseHit = function(obj){
-                                    hit(obj);
-                                }
+    var reverseHit = function(obj){
+        hit(obj);
+    }
 
-                                var isActive = function()
-                                {
-                                    return active;
-                                }
+    var isActive = function()
+    {
+        return active;
+    }
 
-                                return {isActive:isActive,hit:hit, reverseHit:reverseHit, rect:rect,pos:pos, velocity:velocity, vel:velocity, v:velocity, update:update, draw:draw};
+    return {isActive:isActive,hit:hit, reverseHit:reverseHit, rect:rect,pos:pos, velocity:velocity, vel:velocity, v:velocity, update:update, draw:draw};
 }
